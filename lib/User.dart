47d605ec6058da4bbdfb9d4c12bd42b582814a77
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2023-08-28 15:28:38 -0700

    Update User.dart

--- lib/User.dart
+++ lib/User.dart
@@ -7,17 +7,6 @@ class User with ChangeNotifier {
   User(this.name, this.email, this.password);
 }
 
-class UserProvider extends ChangeNotifier {
-  String? _userEmail;
-
-  String? get userEmail => _userEmail;
-
-  void setUserInfo(String email) {
-    _userEmail = email;
-    notifyListeners();
-  }
-}
-
 
 
 
