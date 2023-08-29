633d51d744546f36aab7a5a1359d9b3978c2fda6
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2023-08-28 20:25:36 -0700

    objectified utils

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,6 +1,12 @@
+import 'dart:async';
+
+import 'package:Travis/pages/Map.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart' as geolocator;
+import 'package:intl/intl.dart';
 
 class MyCustomScrollBehavior extends MaterialScrollBehavior {
   @override
@@ -79,4 +85,46 @@ TextStyle SafeGoogleFont(
       decorationThickness: decorationThickness,
     );
   }
+}
+
+class Utils {
+  late Timer timer;
+
+  double calculateDistance(LatLng latLng1, LatLng latLng2) {
+    double distanceInMeters = geolocator.Geolocator.distanceBetween(
+      latLng1.latitude,
+      latLng1.longitude,
+      latLng2.latitude,
+      latLng2.longitude,
+    );
+    return distanceInMeters;
+  }
+
+  String formatTime(milliseconds) {
+    Duration duration = Duration(milliseconds: milliseconds);
+    return DateFormat('HH:mm:ss').format(DateTime(0).add(duration));
+  }
+
+// void startTimer() {
+//   timer = Timer.periodic(const Duration(milliseconds: 10), (Timer timer) {
+//     milliseconds += 10;
+//     setState
+//   });
+// }
+
+// void stopTimer() {
+//   timer.cancel();
+// }
+
+  // void toggleTimer() {
+  //   setState(() {
+  //     isRunning = !isRunning;
+  //     if (isRunning) {
+  //       startTimer();
+  //     } else {
+  //       stopTimer();
+  //     }
+  //   });
+  // }
+
 }
